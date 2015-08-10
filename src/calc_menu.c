9f0de1d24d118c07127c03ef044d7262e1fa3e58
Joseph Roque <github@josephroque.ca>
2015-08-10 08:16:32 -0400

    Fixed label alignment

--- src/calc_menu.c
+++ src/calc_menu.c
@@ -31,31 +31,31 @@ static void initialise_ui(void) {
   s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24);
 
   //Initialising labels
-  s_textlayer_label_subtotal = text_layer_create(GRect(1, 1, 60, 26));
+  s_textlayer_label_subtotal = text_layer_create(GRect(1, 1, 60, 28));
   text_layer_set_text(s_textlayer_label_subtotal, "Subtotal");
   text_layer_set_text_alignment(s_textlayer_label_subtotal, GTextAlignmentRight);
   text_layer_set_font(s_textlayer_label_subtotal, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_label_subtotal);
 
-  s_textlayer_label_service = text_layer_create(GRect(1, 27, 60, 26));
+  s_textlayer_label_service = text_layer_create(GRect(1, 29, 60, 28));
   text_layer_set_text(s_textlayer_label_service, "Service");
   text_layer_set_text_alignment(s_textlayer_label_service, GTextAlignmentRight);
   text_layer_set_font(s_textlayer_label_service, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_label_service);
 
-  s_textlayer_label_tip_pct = text_layer_create(GRect(1, 53, 60, 26));
+  s_textlayer_label_tip_pct = text_layer_create(GRect(1, 57, 60, 28));
   text_layer_set_text(s_textlayer_label_tip_pct, "Tip %");
   text_layer_set_text_alignment(s_textlayer_label_tip_pct, GTextAlignmentRight);
   text_layer_set_font(s_textlayer_label_tip_pct, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_label_tip_pct);
 
-  s_textlayer_label_tip_amt = text_layer_create(GRect(1, 100, 60, 26));
+  s_textlayer_label_tip_amt = text_layer_create(GRect(1, 96, 60, 28));
   text_layer_set_text(s_textlayer_label_tip_amt, "Tip $");
   text_layer_set_text_alignment(s_textlayer_label_tip_amt, GTextAlignmentRight);
   text_layer_set_font(s_textlayer_label_tip_amt, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_label_tip_amt);
 
-  s_textlayer_label_total = text_layer_create(GRect(1, 126, 60, 26));
+  s_textlayer_label_total = text_layer_create(GRect(1, 124, 60, 28));
   text_layer_set_text(s_textlayer_label_total, "Total");
   text_layer_set_text_alignment(s_textlayer_label_total, GTextAlignmentRight);
   text_layer_set_font(s_textlayer_label_total, s_res_gothic_24);
@@ -64,19 +64,19 @@ static void initialise_ui(void) {
   // Initialising outputs
   s_textlayer_subtotal = text_layer_create(GRect(70, 1, 70, 28));
   text_layer_set_text(s_textlayer_subtotal, "$20.00");
-  text_layer_set_text_alignment(s_textlayer_subtotal, GTextAlignmentRight);
+  text_layer_set_text_alignment(s_textlayer_subtotal, GTextAlignmentLeft);
   text_layer_set_font(s_textlayer_subtotal, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_subtotal);
 
   s_textlayer_service = text_layer_create(GRect(70, 29, 70, 28));
   text_layer_set_text(s_textlayer_service, "Avg");
-  text_layer_set_text_alignment(s_textlayer_service, GTextAlignmentRight);
+  text_layer_set_text_alignment(s_textlayer_service, GTextAlignmentLeft);
   text_layer_set_font(s_textlayer_service, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_service);
 
   s_textlayer_tip_pct = text_layer_create(GRect(70, 57, 70, 28));
   text_layer_set_text(s_textlayer_tip_pct, "15%");
-  text_layer_set_text_alignment(s_textlayer_tip_pct, GTextAlignmentRight);
+  text_layer_set_text_alignment(s_textlayer_tip_pct, GTextAlignmentLeft);
   text_layer_set_font(s_textlayer_tip_pct, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_textlayer_tip_pct);
 
